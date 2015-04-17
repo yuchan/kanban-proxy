3970178de7c14d85c377084a468ed9a3ae5fb3f3
yuchan <yusuke@junkpiano.me>
2015-04-17 16:55:18 +0900

    add github auth.

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
-var express = require('express');
+var express = require('express')
+, passport = require('passport')
+, GitHubStrategy = require('passport-github').Strategy;
 
+var GITHUB_CLIENT_ID = "3136a01bff72d87d67c2";
+var GITHUB_CLIENT_SECRET = "e1af21a6626bd3e247e1886eca88bde88f90125d";
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 var app = express();
 app.set('view engine', 'jade');
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
 
 app.get('/', function(req, res){
   res.render('index',   { title : 'Home' });
 });
 
-app.get('/login', function(req, res){
-  res.redirect('/')
-});
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
 
 app.listen(3000);
